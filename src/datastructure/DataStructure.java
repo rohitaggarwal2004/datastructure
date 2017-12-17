e623084da46703bacea66b4e1beafbcb91729ef4
Rohit Aggarwal <rohitaggarwal2004@yahoo.com>
2017-12-17 23:51:03 +0530

    Adding hashtable

--- src/datastructure/DataStructure.java
+++ src/datastructure/DataStructure.java
@@ -15,7 +15,7 @@ public class DataStructure {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("adsads");
     }
-    
+
 }
