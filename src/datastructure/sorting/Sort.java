e71739b364c0b2234735a6732c1d7b77484f1212
Rohit Aggarwal <rohitaggarwal2004@yahoo.com>
2017-12-19 16:03:56 +0530

    Adding hashtable

---
+++ src/datastructure/sorting/Sort.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastructure.sorting;
+
+/**
+ *
+ * @author roaggarw
+ */
+public interface Sort<T> {
+
+    public void sort();
+
+}
